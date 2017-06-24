02f3883c12f2b3ee32dadacea61700330122898f
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-06-24 23:38:05 +0200

    Partially impl. the frame buffer & deleted SM

--- VirtualComputer/src/sznp/virtualcomputer/MCFrameBuffer.java
+++ VirtualComputer/src/sznp/virtualcomputer/MCFrameBuffer.java
@@ -3,91 +3,76 @@ package sznp.virtualcomputer;
 import org.mozilla.interfaces.IFramebuffer;
 import org.mozilla.interfaces.IFramebufferOverlay;
 import org.mozilla.interfaces.nsISupports;
+import org.virtualbox_5_1.BitmapFormat;
+import org.virtualbox_5_1.FramebufferCapabilities;
 
 public class MCFrameBuffer implements IFramebuffer {
 
 	@Override
 	public nsISupports queryInterface(String arg0) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public long getBitsPerPixel() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 32;
 	}
 
 	@Override
 	public long getBytesPerLine() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 640L;
 	}
 
 	@Override
 	public long[] getCapabilities(long[] arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return new long[] { FramebufferCapabilities.UpdateImage.value() };
 	}
 
 	@Override
 	public long getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 480;
 	}
 
 	@Override
 	public long getHeightReduction() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public IFramebufferOverlay getOverlay() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public long getPixelFormat() {
-		// TODO Auto-generated method stub
-		return 0;
+		return BitmapFormat.BGRA.value();
 	}
 
 	@Override
 	public long getVisibleRegion(byte arg0, long arg1) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public long getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 640;
 	}
 
 	@Override
 	public long getWinId() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public void notify3DEvent(long arg0, byte[] arg1) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void notifyChange(long arg0, long arg1, long arg2, long arg3, long arg4) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void notifyUpdate(long arg0, long arg1, long arg2, long arg3) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -97,19 +82,14 @@ public class MCFrameBuffer implements IFramebuffer {
 
 	@Override
 	public void processVHWACommand(byte arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void setVisibleRegion(byte arg0, long arg1) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public boolean videoModeSupported(long arg0, long arg1, long arg2) {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 }
