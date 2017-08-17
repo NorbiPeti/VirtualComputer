5b9a6c5b4ab9129bb69ec57c32e0722e343cf374
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-08-18 00:31:39 +0200

    Fixed /c alias (hopefully)
    Updated readme a while ago
    Deleted CSharp part a while ago

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # The Computer
-A working modern computer in Minecraft made using VirtualBox, jni4net, C# and Java. No clientside mods required.
+A working modern computer in Minecraft made using VirtualBox and Java. No clientside mods required.
 
 I started working on this project on 2015. december 5. However I did not write much code in that time as I had to figure out how things work.
 I plan on making a series where I show the whole process of creating this project.
@@ -14,30 +14,28 @@ I plan on making a series where I show the whole process of creating this projec
 ### Currently compiled for (and tested on):
 *If you don't meet these requirements, you will need to compile the files for yourself. Please help by sending the compiled version to me so more people can download it.*
 
-* Windows 7 64-bit
-* VirtualBox 5
-* Spgiot/Bukkit 1.8/1.9 (with Movecraft for the mouse craft type)
-* Microsoft .NET Framework 4.5
+* OS X
+* VirtualBox 5.1
+* Spgiot/Bukkit 1.8/1.9/1.12
 * Java 8
 
 ### Possibly works with (untested):
-* Any version of Windows which supports .NET 4.5
+* Any operating system which is not Windows
 * Other VirtualBox versions (code modifications are probably necessary)
 * Other Spigot/Bukkit versions
 * Microsoft .NET Framework 4 (code modifications *are* necessary)
 * Other Java versions
 
-*Due to jni4net support limitations the plugin does not support other operating systems.*
+*Due to VirtualBox Java binding support limitations the plugin does not support Windows currently.*
 
 ## Installation
-**Warning:** The first loaded world on the server (the one specified in server.properties) will **lose** the first 20 maps, though it might be only a temporary effect, it might restore after the server starts up without the plugin. *(Untested.)*
+**Warning:** The first loaded world on the server (the one specified in server.properties) will temporarily **lose** the first 20 maps, although it will reset if the server is loaded without the plugin.
 
 * Install all the requirements
 * Add a virtual PC if you don't have any (the first one will be started)
     * Install an OS and Guest Additions, if you haven't already
 * Put the jar file into <server>/plugins directory
-* Put the dll file into <server>/plugins/VirtualComputer directory - if it doesn't exist, create it
-* Make sure your server is set to a world that you don't mind your maps deleted from
+* Make sure your server is set to a world that you don't mind your maps temporarily deleted from
 * Start your server
 
 ## Usage
@@ -55,22 +53,18 @@ Where you'll need to increment 0 for each map.
 It is also recommended to start the virtual machine first so you can distinguish the different parts of the display while placing it.
 
 #### Keyboard
-You can either open the chat keyboard using /computer input key, or you can use /computer key \<keyname\>. You can find key names [here](https://github.com/NorbiPeti/VirtualComputer/blob/master/VirtualComputerSender/VirtualKeys.cs). Note that not all keys are currently supported.
+You can either open the chat keyboard using /computer input key, or you can use /computer key \<keyname\>. You can find key names [here](https://github.com/NorbiPeti/VirtualComputer/blob/directvb/VirtualComputer/src/sznp/virtualcomputer/Scancode.java).
 
 #### Mouse
 You can "lock" the physical mouse to the virtual one with /computer input mouse, or you can move the mouse (including scrolling) with /computer mouse \<dx\> \<dy\> \<dz\> \<dw\>, where dx specifies the amount of pixels to move right, and dy specifies the amount of pixels to move down, while dz and dw specify the scrolling vertically (positive values scroll down) and horizontally.
 
 To set the locked mouse speed, use /computer input mspeed \<integer\>. The difference in look position will be multiplied with this number. Recommended values are between 1 and 10 (inclusive). Default: 1.
 
-You can also use the "mouse" Movecraft craft type to move the mouse, though it's a bit unreliable.
-
 You can press a mouse button with /computer mouse <buttonname>. For example: /computer mouse leftbutton. You can find all the button names [here](https://github.com/NorbiPeti/VirtualComputer/blob/master/VirtualComputerSender/MouseBS.cs).
 
 #### Sounds
-Due to the way it works, it automatically plays every sound from the virtual machine **on the host computer**. This is built into VBoxHeadless, it seems. In order to make the sounds play for every connected player, we'd need a clientside mod.
+Due to the way it works, it automatically plays every sound from the virtual machine **on the host computer**. This is built into the VM, it seems. In order to make the sounds play for every connected player, we'd need a clientside mod.
 
 ### Special thanks:
-* The creators of VirtualBox for making it open-source and easy to use (though I had minor issues but those was because .NET had a few marshaling errors with booleans and that I needed to learn how machines work - especially keyboards)
-* The creators of jni4net, after I figured it out, it was quite easy to use
-* Minecraft player iie for testing and suggesting the Movecraft mouse and other ideas not related to this project
-* The creator(s) of SketchMap, a plugin which I edited to create the display
+* The creators of VirtualBox for making it open-source and kind of easy to use (though I had minor issues as the documentation doesn't really tell me how to write a new frontend :P and that I needed to learn how machines work - especially keyboards)
+* @iiegit for testing and more testing for the non-Windows version
--- VirtualComputer/src/sznp/virtualcomputer/PluginMain.java
+++ VirtualComputer/src/sznp/virtualcomputer/PluginMain.java
@@ -82,6 +82,7 @@ public class PluginMain extends JavaPlugin {
 			session.setName("minecraft");
 			// machine.launchVMProcess(session, "headless", "").waitForCompletion(10000); - This creates a *process*, we don't want that anymore
 			machine.lockMachine(session, LockType.VM); // We want the machine inside *our* process <-- Need the VM type to have console access
+			sender.sendMessage("A: " + machine.getState().toString());
 			screenupdatetask = Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
 				if (session.getState() != SessionState.Locked) // https://www.virtualbox.org/sdkref/_virtual_box_8idl.html#ac82c179a797c0d7c249d1b98a8e3aa8f
 					return; // "This state also occurs as a short transient state during an IMachine::lockMachine call."
@@ -90,20 +91,21 @@ public class PluginMain extends JavaPlugin {
 					screenupdatetask = null;
 				}
 				machine = session.getMachine(); // This is the Machine object we can work with
-				/*
-				 * if (session == null) System.out.println("Session is null"); final IConsole console = session.getConsole(); if (console == null) System.out.println("Console is null"); final
-				 * IProgress powerUp = console.powerUp(); if (powerUp == null) System.out.println("Progess is null"); powerUp.waitForCompletion(10000);
-				 * System.out.println("I DON'T FKIN KNOW WHAT'S NULL - Okay so Console's null as I guessed"); System.out.println("Okay, actually it's good news if this shows.");
-				 */
 				final IConsole console = session.getConsole();
+				sender.sendMessage("1: " + console.getState().toString());
+				console.powerUp().waitForCompletion(10000);
+				sender.sendMessage("2: " + console.getState().toString());
 				console.getDisplay().attachFramebuffer(0L, new IFramebuffer(new MCFrameBuffer(console.getDisplay())));
+				sender.sendMessage("3: " + console.getState().toString());
 				if (screenupdatetask == null)
 					screenupdatetask = Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
+						sender.sendMessage("4: " + console.getState().toString());
 						if (session.getState().equals(SessionState.Locked) // Don't run until the machine is running
 								&& console.getState().equals(MachineState.Running))
 							console.getDisplay().invalidateAndUpdateScreen(0L);
 						if (session.getState().equals(SessionState.Unlocked) // Stop if the machine stopped fully
 								|| console.getState().equals(MachineState.PoweredOff)) {
+							sender.sendMessage("5: " + console.getState().toString());
 							if (session.getState().equals(SessionState.Locked)) {
 								session.unlockMachine();
 								sender.sendMessage("Computer powered off, released it.");
@@ -120,6 +122,8 @@ public class PluginMain extends JavaPlugin {
 	public static int MouseSpeed = 1;
 
 	public void Stop(CommandSender sender) {
+		if (!checkMachineRunning(sender))
+			return;
 		sender.sendMessage("§eStopping computer...");
 		session.getConsole().powerDown().waitForCompletion(2000);
 		session.unlockMachine();
@@ -143,48 +147,58 @@ public class PluginMain extends JavaPlugin {
 	}
 
 	public void Reset(CommandSender sender) {
+		if (!checkMachineRunning(sender))
+			return;
 		sender.sendMessage("§eResetting computer...");
-		if (session.getState() == SessionState.Locked)
-			session.getConsole().reset();
+		session.getConsole().reset();
 		sender.sendMessage("§eComputer reset.");
 	}
 
 	public void FixScreen(CommandSender sender) {
+		if (!checkMachineRunning(sender))
+			return;
 		sender.sendMessage("§eFixing screen...");
 		session.getConsole().getDisplay().setSeamlessMode(false);
 		session.getConsole().getDisplay().setVideoModeHint(0L, true, false, 0, 0, 640L, 480L, 32L);
 		sender.sendMessage("§eScreen fixed.");
 	}
 
+	private boolean checkMachineRunning(CommandSender sender) {
+		if (session.getState() != SessionState.Locked || machine.getState() != MachineState.Running) {
+			sender.sendMessage("§cMachine isn't running.");
+			return false;
+		}
+		return true;
+	}
+
 	public void PressKey(CommandSender sender, String key, String stateorduration) {
-		if (session.getState() == SessionState.Locked) {
-			int durationorstate;
-			if (stateorduration.length() == 0)
-				durationorstate = 0;
-			else if (stateorduration.equalsIgnoreCase("down"))
-				durationorstate = -1;
-			else if (stateorduration.equalsIgnoreCase("up"))
-				durationorstate = -2;
-			else
-				durationorstate = Short.parseShort(stateorduration);
-			int code = 0;
-			// Release key scan code concept taken from VirtualBox source code (KeyboardImpl.cpp:putCAD())
-			// +128
-			if (durationorstate != 2)
-				session.getConsole().getKeyboard().putScancode(code);
-			Runnable sendrelease = () -> session.getConsole().getKeyboard()
-					.putScancodes(Lists.newArrayList(code + 128, Scancode.sc_controlLeft.Code + 128,
-							Scancode.sc_shiftLeft.Code + 128, Scancode.sc_altLeft.Code + 128));
-			if (durationorstate == 0 || durationorstate == -2)
-				sendrelease.run();
-			if (durationorstate > 0) {
-				Bukkit.getScheduler().runTaskLaterAsynchronously(this, sendrelease, durationorstate);
-			}
+		if (!checkMachineRunning(sender))
+			return;
+		int durationorstate;
+		if (stateorduration.length() == 0)
+			durationorstate = 0;
+		else if (stateorduration.equalsIgnoreCase("down"))
+			durationorstate = -1;
+		else if (stateorduration.equalsIgnoreCase("up"))
+			durationorstate = -2;
+		else
+			durationorstate = Short.parseShort(stateorduration);
+		int code = 0;
+		// Release key scan code concept taken from VirtualBox source code (KeyboardImpl.cpp:putCAD())
+		// +128
+		if (durationorstate != 2)
+			session.getConsole().getKeyboard().putScancode(code);
+		Runnable sendrelease = () -> session.getConsole().getKeyboard().putScancodes(Lists.newArrayList(code + 128,
+				Scancode.sc_controlLeft.Code + 128, Scancode.sc_shiftLeft.Code + 128, Scancode.sc_altLeft.Code + 128));
+		if (durationorstate == 0 || durationorstate == -2)
+			sendrelease.run();
+		if (durationorstate > 0) {
+			Bukkit.getScheduler().runTaskLaterAsynchronously(this, sendrelease, durationorstate);
 		}
 	}
 
 	public void UpdateMouse(CommandSender sender, int x, int y, int z, int w, String mbs, boolean down) {
-		if (session.getState() != SessionState.Locked)
+		if (!checkMachineRunning(sender))
 			return;
 		int state = 0;
 		if (mbs.length() > 0 && down)
@@ -194,6 +208,8 @@ public class PluginMain extends JavaPlugin {
 	}
 
 	public void UpdateMouse(CommandSender sender, int x, int y, int z, int w, String mbs) {
+		if (!checkMachineRunning(sender))
+			return;
 		UpdateMouse(sender, x, y, z, w, mbs, true);
 		UpdateMouse(sender, x, y, z, w, mbs, false);
 	}
