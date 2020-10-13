a32f80fb79d1d6e537255fed779ae1ade4c91010
NorbiPeti <szatmari.norbert.peter@gmail.com>
2020-10-13 02:57:35 +0200

    Converted and improved the commands

--- VirtualComputer-Core/src/main/java/sznp/virtualcomputer/ComputerCommand.java
+++ VirtualComputer-Core/src/main/java/sznp/virtualcomputer/ComputerCommand.java
@@ -3,13 +3,22 @@ package sznp.virtualcomputer;
 import buttondevteam.lib.chat.Command2;
 import buttondevteam.lib.chat.CommandClass;
 import buttondevteam.lib.chat.ICommand2MC;
+import lombok.var;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.javatuples.Tuple;
 import org.virtualbox_6_1.VBoxException;
+
+import javax.annotation.Nullable;
 
 @CommandClass
 public class ComputerCommand extends ICommand2MC {
 	@Command2.Subcommand
-	public void def(CommandSender sender) {
+	public void status(CommandSender sender) {
+		//TODO
 	}
 
 	@Command2.Subcommand(aliases = {"poweron", "on"})
@@ -71,5 +80,58 @@ public class ComputerCommand extends ICommand2MC {
 			throw e;
 		} catch (Exception ignored) {
 		}
+	}
+
+	@Command2.Subcommand(helpText = {
+			"Show keyboard",
+			"Displays a keyboard in chat that you can click on.",
+			"Each keypress is actually a command so they will be logged by the server."
+	})
+	public void show_keyboard(CommandSender sender) {
+		sender.sendMessage("§6Avoid entering sensitive info here unless you own the server");
+		var cc = new TextComponent();
+		for (int i = 0; i < 10; i++)
+			cc.addExtra(keyComp(i + "", null));
+		var row = new String[]{"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"};
+		for (String s : row) cc.addExtra(keyComp(s, null));
+		cc.addExtra(keyComp("-", "minus"));
+		cc.addExtra(keyComp("=", "equals"));
+		cc.addExtra(keyComp("Backspace", "backspace"));
+		cc.addExtra(keyComp("Tab", "tab"));
+		sender.spigot().sendMessage(cc);
+
+		cc = new TextComponent();
+		row = new String[]{"A", "S", "D", "F", "G", "H", "J", "K", "L"};
+		for (String s : row) cc.addExtra(keyComp(s, null)); //TODO: File format for layouts
+
+		cc.addExtra(keyComp("{", "bracketLeft"));
+		cc.addExtra(keyComp("}", "bracketRight"));
+
+		sender.spigot().sendMessage(cc);
+	}
+
+	private TextComponent keyComp(String name, @Nullable String code) {
+		if (code == null) code = name;
+		var tc = new TextComponent("[" + name + "] ");
+		tc.setColor(ChatColor.AQUA);
+		tc.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/c key " + code));
+		return tc;
+	}
+
+	@Command2.Subcommand(helpText = {
+			"Lock mouse",
+			"Locks the mouse to where you're looking. If you move your mouse, the computer's mouse will be moved."
+	})
+	public void lock_mouse(Player player) {
+		MouseLockerPlayerListener.toggleLock(player);
+	}
+
+	@Command2.Subcommand(helpText = {
+			"Set mouse speed",
+			"Sets the mouse speed when locked. The default is 5."
+	})
+	public void lock_speed(CommandSender sender, float speed) {
+		MouseLockerPlayerListener.LockedSpeed = speed;
+		sender.sendMessage("§aMouse speed set to " + MouseLockerPlayerListener.LockedSpeed);
 	}
 }
